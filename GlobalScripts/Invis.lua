6c1117348273fe05af7556551a76a28cca8f8c68
DankBladeZS <59133811+DankBladeZS@users.noreply.github.com>
2022-02-10 16:52:51 +0300

    Update Invis.lua

--- GlobalScripts/Invis.lua
+++ GlobalScripts/Invis.lua
@@ -6,14 +6,15 @@ if plr.Character.Humanoid.RigType == Enum.HumanoidRigType.R15 then
 elseif plr.Character.Humanoid.RigType == Enum.HumanoidRigType.R6 then
     joint = plr.Character.HumanoidRootPart.RootJoint
 end 
-
+if useplatform then
 local part = Instance.new("Part", workspace)
 part.Anchored = true
 part.CanCollide = true
 part.Size = Vector3.new(100, 5, 100)
 part.CFrame = CFrame.new(10000, 10000-10, 10000)
+end
 wait(0.1)
-plr.Character.HumanoidRootPart.CFrame = CFrame.new(10000, 10000, 10000)
+plr.Character.HumanoidRootPart.CFrame = invispos
 wait(0.5)
 joint.Parent = game.ReplicatedStorage
 wait(0.5)
