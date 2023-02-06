72fbd15bc620b4bd5596463ef2dc08908c24ecd0
DankBladeZS <59133811+DankBladeZS@users.noreply.github.com>
2023-02-06 11:37:12 +0300

    Update AFKWorldServerhop.lua

--- ABA/AFKWorldServerhop.lua
+++ ABA/AFKWorldServerhop.lua
@@ -4,6 +4,7 @@ if game.PlaceId == 5411459567 then
 
 local message = game:GetService("Workspace"):WaitForChild("Message").Text
 local number = message:match("%d+")
+local number = tonumber(number)
 
 if number < getgenv().MaxTime then
     wait(number + getgenv().PayoutDelay)
