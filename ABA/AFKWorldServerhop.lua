5a328878e3d58949a76356a3f863712e70f5226a
DankBladeZS <59133811+DankBladeZS@users.noreply.github.com>
2023-02-06 11:35:33 +0300

    Update AFKWorldServerhop.lua

--- ABA/AFKWorldServerhop.lua
+++ ABA/AFKWorldServerhop.lua
@@ -1,6 +1,8 @@
 repeat wait() until game:IsLoaded()
 
-local message = game:GetService("Workspace").Message.Text
+if game.PlaceId == 5411459567 then
+
+local message = game:GetService("Workspace"):WaitForChild("Message").Text
 local number = message:match("%d+")
 
 if number < getgenv().MaxTime then
@@ -8,3 +10,5 @@ if number < getgenv().MaxTime then
 end
 
 loadstring(game:HttpGet("https://pastebin.com/raw/qc2SwaaA"))()
+
+end
