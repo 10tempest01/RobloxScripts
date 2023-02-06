8f332d2208a89399594a15834919b67df5583207
DankBladeZS <59133811+DankBladeZS@users.noreply.github.com>
2023-02-06 11:51:26 +0300

    Update AFKWorldServerhop.lua

--- ABA/AFKWorldServerhop.lua
+++ ABA/AFKWorldServerhop.lua
@@ -2,8 +2,8 @@ repeat wait() until game:IsLoaded()
 
 if game.PlaceId == 5411459567 then
 
-local plr = game.Players.LocalPlayer
-local gold = plr:WaitForChild("PlayerGui"):WaitForChild("ScreenGui"):WaitForChild("TextLabel").Text
+local plr = game:GetService("Players").LocalPlayer
+local gold = plr.PlayerGui:WaitForChild("ScreenGui").TextLabel
 local message = game:GetService("Workspace"):WaitForChild("Message").Text
 local number = message:match("%d+")
 local number = tonumber(number)
@@ -12,7 +12,7 @@ if number < getgenv().MaxTime then
     wait(number + getgenv().PayoutDelay)
 end
 
-gold = gold .. "\nServerhopping..."
+gold.Text = gold.Text .. "\nServerhopping..."
 loadstring(game:HttpGet("https://pastebin.com/raw/qc2SwaaA"))()
 
 end
