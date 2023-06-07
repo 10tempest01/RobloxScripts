ad82e27fae7ea2e2dd219332d12c73f35a28d918
DankBladeZS <59133811+DankBladeZS@users.noreply.github.com>
2023-06-07 20:18:28 +0300

    Update AFKWorldServerhop.lua

--- ABA/AFKWorldServerhop.lua
+++ ABA/AFKWorldServerhop.lua
@@ -10,25 +10,24 @@ local number = message:match("%d+")
 local number = tonumber(number)
 
 realgold = gold.Text
-gold.Text = realgold .. "\nWaiting for payout...\nSponsored by Nova Hub "
+gold.Text = realgold .. "\nWaiting for payout...\nSponsored by Nova Hub 🌌"
+
+numberlol = math.random(1, 10)
+if numberlol == 1 then
+    pcall(function()
+        local Image = Drawing.new("Image")
+        local Full = workspace.CurrentCamera.ViewportSize
+        Image.Size = Vector2.new(750, 250)
+        Image.Position = Vector2.new(600, 750)
+        Image.Data = game:HttpGet("https://i.imgur.com/FkKC8AR.jpg")
+        Image.Visible = true
+    end)
+end
 
 if number < getgenv().MaxTime then
     wait(number + getgenv().PayoutDelay)
 end
     
-    
-numberlol = math.random(1, 10)
-if numberlol == 1 then
-        pcall(function()
-                local Image = Drawing.new("Image")
-                local Full = workspace.CurrentCamera.ViewportSize
-                Image.Size = Vector2.new(750, 250)
-                Image.Position = Vector2.new(600, 750)
-                Image.Data = game:HttpGet("https://i.imgur.com/FkKC8AR.jpg")
-                Image.Visible = true
-                end)
-        end
-                
     
 msg = "YOU AINT GETTIN NO GOLD 😭😭😭"
     
