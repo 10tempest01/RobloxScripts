87b494cde2c38545ac899e42aeae283a2d2f07cc
DankBladeZS <59133811+DankBladeZS@users.noreply.github.com>
2023-06-26 16:02:53 +0300

    Create ChestHopper.lua

---
+++ AUT/ChestHopper.lua
@@ -0,0 +1,52 @@
+repeat wait() until game:IsLoaded()
+
+local plr = game:GetService("Players").LocalPlayer
+
+for _, chestSpawn in pairs(game:GetService("Workspace").ItemSpawns.Chests:GetChildren()) do
+	if chestSpawn:FindFirstChild("Chest") then
+		
+		--Chest checks
+		local chest = chestSpawn:FindFirstChild("Chest")
+		local highlight = Instance.new("Highlight", chest)
+		
+		repeat wait() until chest.RootPart:FindFirstChild("ProximityAttachment") and chest.RootPart:FindFirstChild("ProximityAttachment"):FindFirstChild("Interaction") or not chest
+		local proxAttachment = chest.RootPart:FindFirstChild("ProximityAttachment")
+		local proxPrompt = chest.RootPart:FindFirstChild("ProximityAttachment"):FindFirstChild("Interaction")
+		--
+		
+		--Character check
+		local char = plr.Character or plr.CharacterAdded:Wait()
+		
+		--TP and fire ProximityPrompt
+		if char and char:FindFirstChild("HumanoidRootPart") then
+			local hrp = char:FindFirstChild("HumanoidRootPart")
+			repeat wait(0.2)
+				if chest:FindFirstChild("RootPart") then
+					hrp.CFrame = chest.RootPart.CFrame + Vector3.new(0, 2, 0)
+				end
+				fireproximityprompt(proxPrompt)
+			until (not proxAttachment) or (not proxPrompt) or (not proxAttachment and not proxPrompt) or not chest:FindFirstChild("RootPart")
+		end
+		--
+	end
+end
+
+local function store()
+	
+	if plr.Character then
+		for _, tool in pairs(plr.Backpack:GetChildren()) do
+			if getgenv().ItemsToStore[tool.Name] == true then
+				tool.Parent = plr.Character
+			end
+		end
+	end
+	
+	wait(0.5)
+	
+	game:GetService("ReplicatedStorage"):WaitForChild("ReplicatedModules"):WaitForChild("KnitPackage"):WaitForChild("Knit"):WaitForChild("Services"):WaitForChild("InventoryService"):WaitForChild("RE"):WaitForChild("ItemInventory"):FireServer({["AddItems"] = true})
+end
+
+store()
+wait(4)
+
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/DankBladeZS/RobloxScripts/main/GlobalScripts/Serverhop.lua"))()
